80e7d5a842be2577fba85fbb02cb9d71c4f6c408
HuangYu123321 <cuo_061125@qq.com>
2026-05-05 02:02:45 +0800

    error2

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -3,9 +3,31 @@ class AcGameMenu {
         this.root=root
         this.$menu=$(`
 <div class="ac-game-menu">
+<div class="ac-game-menu-field">
+
+<div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+单人模式
+</div>
+
+<br>
+
+<div class="ac-game-menu-field-item ac-game-menu-field-item-multi">
+多人模式
+</div>
+
+<br>
+
+<div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+设置
+</div>
+
+</div>
 </div>
  `)
    this.root.$ac_game.append(this.$menu);
+        this.$single=this.$menu.find('.ac-game-menu-field-item-single');
+        this.$multi=this.$menu.find('.ac-game-menu-field-item-multi');
+        this.$settings=this.$menu.find('.ac-game-menu-field-item-settings');
     }
 }
 class AcGame {
