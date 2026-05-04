55f41c039738405c59035fbba5d920bc6e7094c2
HuangYu123321 <cuo_061125@qq.com>
2026-05-05 02:44:52 +0800

    finished lesson three

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -5,13 +5,13 @@ class AcGameMenu {
 <div class="ac-game-menu">
 <div class="ac-game-menu-field">
 
-<div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+<div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
 单人模式
 </div>
 
 <br>
 
-<div class="ac-game-menu-field-item ac-game-menu-field-item-multi">
+<div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
 多人模式
 </div>
 
@@ -25,9 +25,63 @@ class AcGameMenu {
 </div>
  `)
    this.root.$ac_game.append(this.$menu);
-        this.$single=this.$menu.find('.ac-game-menu-field-item-single');
-        this.$multi=this.$menu.find('.ac-game-menu-field-item-multi');
+        this.$single_mode=this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode=this.$menu.find('.ac-game-menu-field-item-multi-mode');
         this.$settings=this.$menu.find('.ac-game-menu-field-item-settings');
+
+        this.start();
+    }
+
+    start()
+    {
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        let outer=this;
+           this.$single_mode.click(function(){
+                 outer.hide();
+               outer.root.playground.show();
+           });
+
+        this.$multi_mode.click(function(){
+                 console.log("multi");
+        });
+
+        this.$settings.click(function(){
+                 console.log("setting");
+        });
+    }
+
+    show() {  //显示menu界面
+         this.$menu.show();
+    }
+
+    hide() { //关闭menu界面
+         this.$menu.hide();
+    }
+}
+class AcGamePlayground {
+   constructor(root) {
+     this.root=root;
+       this.$playground=$(`<div>游戏界面</div>`);
+       this.hide();
+       this.root.$ac_game.append(this.$playground);
+
+       this.start();
+
+   }
+
+    start() {
+
+    }
+
+    show() { //关闭playground界面
+      this.$playground.show();
+    }
+
+    hide() {  //关闭playground界面
+      this.$playground.hide();
     }
 }
 class AcGame {
@@ -35,5 +89,11 @@ class AcGame {
       this.id=id;
       this.$ac_game=$('#'+id);
       this.menu=new AcGameMenu(this);
+      this.playground=new AcGamePlayground(this);
+
+      this.start();
   }
+
+    start() {
+    }
 }
